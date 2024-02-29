522a25a21d20336c1c7890c69ab57af19b7b6105
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-02-29 08:48:51 +0530

    feb 29

--- day_32 feb_29/app.js
+++ day_32 feb_29/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const userRouter = require('./user')
-const productRouter = require('./product')
+const productRouter = require('./product');
 
 const app = express();
+
+var corsOptions = {
+    origin: 'https://www.w3schools.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 app.use('/users', userRouter);
 app.use('/products', productRouter);
